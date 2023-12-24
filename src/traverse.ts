56a30b72e6d690128da9d2893c82d550ed394c7e
Daisho Komiyama <daishokomiyama@gmail.com>
2023-12-24 13:56:19 -0500

    Clean up

--- src/traverse.ts
+++ src/traverse.ts
@@ -10,49 +10,24 @@ export default function traverse(ast: Node[]) {
         if (_.type === 'List') {
             let item = _ as List | null
 
-            console.log('==>', 100, item)
-            console.log('==>', 100.5, listStack)
+            if (item === null) return
 
             while (listStack.at(-1) && listStack.at(-1)!.depth > item.depth) {
-                console.log('==>', 101)
                 listStack.pop()
             }
 
-            // while (listStack.at(-1) && listStack.at(-1)!.depth === item.depth) {
-            //     console.log('==>', 200, listStack)
-            //     const last = listStack.pop()!
-            //     last.children.push(...item.children)
-            //     listStack.push(last)
-            // }
-
             if (!listStack.length) {
-                console.log('==>', 102, item)
                 result.push(item)
             } else {
                 const parent = listStack.at(-1)!
-                console.log('==>', 103, parent)
 
                 if (parent.depth === item.depth) {
-                    console.log('==>', 104, listStack)
                     parent.children.push(...item.children)
-                    console.log('==>', 104.5, parent)
-
-                    // listStack.push(parent)
-
-                    console.log('==>', 104.75, listStack)
-
-                    // parent.children.push(item)
                     item = null
                 } else {
-                    console.log('==>', 105, parent, item)
-                    console.log('==>', 106, parent)
-
                     parent.children.push(item)
-                    console.log('==>', 107, result)
                 }
             }
-
-            // console.log('==>', 108, item)
 
             if (item) {
                 listStack.push(item)
