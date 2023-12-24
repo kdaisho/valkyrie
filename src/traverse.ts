e7e6ed69f3dbc93cc22d573e2a154b0cb3d6b36a
Daisho Komiyama <daishokomiyama@gmail.com>
2023-12-24 09:43:23 -0500

    Add type

--- src/traverse.ts
+++ src/traverse.ts
@@ -1,48 +1,34 @@
-import { getLeafNode } from './utils'
-
-type LexicalBlock = {
+type Node = {
     type: string
     value: string
-    depth: number
-    children?: LexicalBlock[]
-}
-
-type List = {
+    children: Node[]
+} & {
     type: 'List'
-    value: string
     depth: number
-    children?: (LexicalBlock | List)[]
 }
 
-export default function traverse(ast: (LexicalBlock | List)[]) {
-    function nestLists(arr) {
-        const result = []
-        const listStack = []
+export default function traverse(ast: Node[]) {
+    const result: Node[] = []
+    const listStack: (Node & { depth: number })[] = []
 
-        arr.forEach(item => {
-            if (item.type === 'List') {
-                while (
-                    listStack.length > 0 &&
-                    listStack.at(-1).depth >= item.depth
-                ) {
-                    listStack.pop()
-                }
-
-                if (listStack.length === 0) {
-                    result.push(item)
-                } else {
-                    const parent = listStack.at(-1)
-                    parent.children.push(item)
-                }
-
-                listStack.push(item)
-            } else {
-                result.push(item)
+    ast.forEach(item => {
+        if (item.type === 'List') {
+            while (listStack.at(-1) && listStack.at(-1)!.depth >= item.depth) {
+                listStack.pop()
             }
-        })
 
-        return result
-    }
+            if (!listStack.length) {
+                result.push(item)
+            } else {
+                const parent = listStack.at(-1)!
+                parent.children.push(item)
+            }
 
-    return nestLists(ast)
+            listStack.push(item)
+        } else {
+            result.push(item)
+        }
+    })
+
+    return result
 }
